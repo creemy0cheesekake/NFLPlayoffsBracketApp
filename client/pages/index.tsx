7e84812859f4f4918f4116528c4bc4fa2023cba0
Arjun Anand <arjunanand1405@gmail.com>
2022-01-23 01:17:48 +0000

    fixed dnd. error was components have to be mounted before rendering or else dnd wont work

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from "react";
 import { DragDropContext, DropResult } from "react-beautiful-dnd";
 import Loading from "../components/Loading";
 import styles from "../styles/Home.module.sass";
@@ -6,18 +7,26 @@ import TeamRankings from "./../components/TeamRankings";
 
 // TODO: create component that shows playoff bracket
 const Home = () => {
+	// if components arent mounted by the time the dnd elements are rendered, they wont work
+	const [componentsMounted, setComponentsMounted] = useState(false);
+	useEffect(() => setComponentsMounted(true));
 	const handleDragEnd = (result: DropResult) => {
 		if (!result.destination) return;
 		console.log("hi");
 	};
 	return (
-		<main className={styles.main}>
-			<DragDropContext onDragEnd={handleDragEnd}>
-				{/* <Loading show /> */}
-				<DragDropBox />
-				<TeamRankings />
-			</DragDropContext>
-		</main>
+		<>
+			<main className={styles.main}>
+				{componentsMounted ? (
+					<DragDropContext onDragEnd={handleDragEnd}>
+						<DragDropBox />
+						<TeamRankings />
+					</DragDropContext>
+				) : (
+					<Loading show />
+				)}
+			</main>
+		</>
 	);
 };
 
